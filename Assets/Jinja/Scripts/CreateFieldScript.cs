1c5436e96e3c84ffb07588550c7a4b1d25920e56
junki.hiroi <junki.hiroi@gmail.com>
2017-11-13 23:43:20 +0900

    init Jinja

---
+++ Assets/Jinja/Scripts/CreateFieldScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ghost.Scripts
+{
+public class CreateFieldScript
+{
+    public struct FieldInfo
+    {
+        public List<int> Masu;
+        public int Height;
+        public int Width;
+    }
+
+    public static void CreateField(FieldInfo fieldInfo)
+    {
+        var obake = Resources.Load<GameObject>("Obake");
+        var wall = Resources.Load<GameObject>("Wall");
+        var yuka = Resources.Load<GameObject>("YukaBox");
+        var player = Resources.Load<GameObject>("Player");
+
+        var fieldRoot = new GameObject("FieldRoot");
+
+        for (int y = 0; y < fieldInfo.Height; y++)
+        {
+            for (int x = 0; x < fieldInfo.Width; x++)
+            {
+                int i = y * fieldInfo.Width + x;
+
+                {
+                    var gameObject = GameObject.Instantiate(yuka);
+                    var scaleY = gameObject.transform.localScale.y;
+                    gameObject.transform.localPosition = new Vector3(x, -scaleY / 2, y);
+                    gameObject.transform.parent = fieldRoot.transform;
+                }
+
+                if (5 < fieldInfo.Masu[i] && fieldInfo.Masu[i] < 9)
+                {
+                    var gameObject = GameObject.Instantiate(wall);
+                    var scaleY = gameObject.transform.localScale.y;
+                    gameObject.transform.localPosition = new Vector3(x, scaleY / 2, y);
+                    gameObject.transform.parent = fieldRoot.transform;
+                }
+                else if (fieldInfo.Masu[i] == 9)
+                {
+                    var gameObject = GameObject.Instantiate(obake);
+                    var scaleY = gameObject.transform.localScale.y;
+                    gameObject.transform.localPosition = new Vector3(x, scaleY / 2, y);
+                    gameObject.transform.parent = fieldRoot.transform;
+                }
+                else if (fieldInfo.Masu[i] == 99)
+                {
+                    var gameObject = GameObject.Instantiate(player);
+                    var scaleY = gameObject.transform.localScale.y;
+                    gameObject.transform.localPosition = new Vector3(x, scaleY / 2, y);
+                    gameObject.transform.parent = fieldRoot.transform;
+                }
+            }
+        }
+
+    }
+}
+}
---
+++ Assets/Jinja/Scripts/GhostAppManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Ghost.Scripts
+{
+public class GhostAppManager : MonoBehaviour
+{
+    private GameObject cameraGameObject;
+    private GameObject playerGameObject;
+    private int frameCount = 0;
+
+    private void Start ()
+    {
+        CreateFieldScript.FieldInfo fieldInfo = new CreateFieldScript.FieldInfo();
+
+        fieldInfo.Masu = new List<int>();
+        fieldInfo.Height = 10;
+        fieldInfo.Width = 10;
+
+        var playerIndex = Random.Range(0, fieldInfo.Height * fieldInfo.Width);
+
+        for (int i = 0; i < fieldInfo.Height * fieldInfo.Width; i++)
+        {
+            var fieldProperty = Random.Range(0, 10);
+
+            if (i == playerIndex)
+            {
+                fieldProperty = 99;
+            }
+
+            fieldInfo.Masu.Add(fieldProperty);
+        }
+
+        CreateFieldScript.CreateField(fieldInfo);
+
+        cameraGameObject = GameObject.FindWithTag("MainCamera");
+        playerGameObject = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (frameCount == 0)
+        {
+            var dx = (int)Input.GetAxisRaw("Horizontal");
+            var dy = (int)Input.GetAxisRaw("Vertical");
+
+            if (Math.Abs(dx) + Math.Abs(dy) == 1)
+            {
+                playerGameObject.transform.Translate(dx, 0, dy);
+                frameCount = 20;
+            }
+        }
+        else
+        {
+            frameCount--;
+        }
+
+        cameraGameObject.transform.position = playerGameObject.transform.position + new Vector3(0, 10, -6);
+        cameraGameObject.transform.LookAt(playerGameObject.transform);
+    }
+}
+}
