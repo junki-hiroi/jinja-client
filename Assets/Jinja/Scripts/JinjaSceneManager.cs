074104173def4fe4831d2f0fe81eae4c2f7e85b5
junki.hiroi <junki.hiroi@gmail.com>
2017-11-20 22:46:35 +0900

    add JinjaSceneManager
    typo

---
+++ Assets/Jinja/Scripts/JinjaSceneManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Jinja.Scripts
+{
+public class JinjaSceneManager
+{
+    public const int Main = 0;
+    public const int MapSelect = 1;
+
+
+    private static JinjaSceneManager _instanse = null;
+    private AsyncOperation _asyncOperation = null;
+
+    public AsyncOperation LatestAsyncOperation
+    {
+        get { return _asyncOperation; }
+    }
+
+    public static JinjaSceneManager Instanse
+    {
+        get
+        {
+            if (_instanse == null)
+            {
+                _instanse = new JinjaSceneManager();
+            }
+
+            return _instanse;
+        }
+    }
+
+    public bool RequestLoadSceneAsync(int sceneBuildIndex, LoadSceneMode loadSceneMode)
+    {
+        if (_asyncOperation != null)
+        {
+            if (!_asyncOperation.isDone)
+            {
+                return false;
+            }
+        }
+
+        _asyncOperation = SceneManager.LoadSceneAsync(sceneBuildIndex, loadSceneMode);
+        return true;
+    }
+}
+}
