b52e9c65941d9b3e4a0758adb01ff02a5dceef3d
junki.hiroi <junki.hiroi@gmail.com>
2017-11-20 21:55:15 +0900

    マップセレクトシーン追加

---
+++ Assets/Jinja/Scripts/MapSelect.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Jinja.Scripts
+{
+public class MapSelect : MonoBehaviour
+{
+    private Func<bool>[] _buttonPressing;
+
+    void Start ()
+    {
+        var canvas = GameObject.Find("Canvas");
+        canvas.GetComponent<RectTransform>().localScale = Vector3.one;
+        _buttonPressing = new Func<bool>[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            var button = new GameObject("button" + i);
+            button.transform.parent = canvas.transform;
+            var rectTransform = button.AddComponent<RectTransform>();
+            rectTransform.anchoredPosition3D = Vector3.zero;
+            rectTransform.sizeDelta = Vector2.zero;
+
+            Vector2 centerPosition = new Vector2(0.5f, 0.8f) +
+            new Vector2(
+                0.0f,
+                - i * 0.25f
+            );
+
+            rectTransform.anchorMin = centerPosition - Vector2.one * 0.1f;
+            rectTransform.anchorMax = centerPosition + Vector2.one * 0.1f;
+
+            button.AddComponent<CanvasRenderer>();
+            var image = button.AddComponent<Image>();
+#if UNITY_EDITOR
+            image.sprite = UnityEditor.AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
+#else
+            image.sprite = Resources.GetBuiltinResource<Sprite>("UI/Skin/UISprite.psd");
+#endif
+            image.type = Image.Type.Sliced;
+
+            var pressedButton = button.AddComponent<PressedButton>();
+            _buttonPressing[i] = () => pressedButton.IsPressed;
+        }
+    }
+
+    void Update ()
+    {
+        int findIndex = Array.FindIndex(_buttonPressing, func => func());
+
+        if (findIndex != -1)
+        {
+            SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+        }
+    }
+}
+}
