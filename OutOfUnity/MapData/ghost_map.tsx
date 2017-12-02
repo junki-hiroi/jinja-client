4952cce94b74b11f6d083d7192f0c8e9946023fc
junki.hiroi <junki.hiroi@gmail.com>
2017-12-03 02:14:23 +0900

    旧データを削除

--- OutOfUnity/MapData/ghost_map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="basictiles" tilewidth="16" tileheight="16" tilecount="120" columns="8">
- <image source="basictiles.png" trans="000000" width="128" height="240"/>
-</tileset>
